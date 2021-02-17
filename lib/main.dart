507361733c09568246d2dd591c2fc50e3eeee564
LOZSPEED <70553151+VANLIM131@users.noreply.github.com>
2021-02-17 15:54:51 +0700

    #11
    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,108 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'pokemon.dart';
+import 'pokemondetail.dart';
 
 void main() => runApp(MaterialApp(
-      title: "pokedex",
+      title: "PokeDex",
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     ));
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() {
+    return new HomePageState();
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   var url =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+
+  PokeHub pokeHub;
 
   @override
   void initState() {
     super.initState();
+
     fetchData();
-    print("2nd work");
   }
 
   fetchData() async {
-    var res =
-        await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
-    print(res.body);
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(decodedJson);
+    print(pokeHub.toJson());
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("pokedux"),
-          backgroundColor: Colors.grey,
-        ),
-        body: Center(
-          child: Text("hellow"),
-        ),
-        drawer: Drawer(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.cyan,
-          child: Icon(
-            Icons.refresh,
-          ),
-        ));
+      appBar: AppBar(
+        title: Text("PokeDex"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PokeDetail(
+                                          pokemon: poke,
+                                        )));
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.4,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.2,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: NetworkImage(poke.img))),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey[300],
+        child: Icon(Icons.refresh),
+      ),
+    );
   }
 }
